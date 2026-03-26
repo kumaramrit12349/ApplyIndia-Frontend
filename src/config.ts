0357b9568eda549b0b922013e453effc42eaefb3
ApplyIndia <support@applyindia.online>
2026-03-26 15:38:16 +0530

    done some fixes.

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,6 @@
 // src/config.ts
-console.log("ENV-1:", import.meta.env.VITE_ENV);
-console.log("API-1:", import.meta.env.VITE_API_BASE_URL);
 export const CONFIG = {
-    
+
   API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
   ENV: import.meta.env.VITE_ENV,
 };
